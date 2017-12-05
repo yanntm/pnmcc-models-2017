8e6b8051b36a60b236db1c5a68c573effc0ae1c9
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-05 18:22:32 +0100

    support scenario where INPUTs is partly filled in

--- install_inputs.sh
+++ install_inputs.sh
@@ -2,26 +2,31 @@
 
 set -x
 
-if [ ! -d INPUTS ] ; then 
-    
-    for i in MCC-INPUTS.tgz ;
-    do 
-	if [ ! -f $i ] ; then 
-	    wget --progress=dot:mega http://mcc.lip6.fr/archives/$i
-	fi
-	tar xzf $i
-	rm -f $i
-    done
-    mv BenchKit/INPUTS .
-    \rm -r BenchKit
-    
-    mkdir test
-	cd test
-	cp ../scalar.tgz .
-	tar xzf scalar.tgz
-	cd ../INPUTS
-	for i in $(ls -1 ../test/scalar); do tar xzf $i.tgz && cp ../test/scalar/$i/* $i/ && \rm $i.tgz && tar czf $i.tgz $i/ && rm -rf $i/ ; done ;
-	cd ..
-	\rm -rf scalar/    
-fi
+
+for i in MCC-INPUTS.tgz ;
+do 
+    if [ ! -f $i ] ; then 
+	wget --progress=dot:mega http://mcc.lip6.fr/archives/$i
+    fi
+    tar xzf $i
+    rm -f $i
+done
+mv BenchKit/INPUTS/* ./INPUTS/
+\rm -r BenchKit
+
+mkdir test
+cd test
+cp ../scalar.tgz .
+tar xzf scalar.tgz
+cd ../INPUTS
+for i in $(ls -1 ../test/scalar); do
+    if [ -f $i.tgz ] ;
+    then
+	tar xzf $i.tgz && cp ../test/scalar/$i/* $i/ && \rm $i.tgz && tar czf $i.tgz $i/ && rm -rf $i/ ;
+    fi
+done
+
+cd ..
+\rm -rf scalar/    
+
 
